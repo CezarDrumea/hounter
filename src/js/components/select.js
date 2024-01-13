9b06b668a013bfb546015f57ea9a336e84f067f0
Cezar Drumea <cdrumea.griddynamics@gmail.com>
2024-01-13 13:59:26 +0200

    [WIP] add join now section

--- src/js/components/select.js
+++ src/js/components/select.js
@@ -1,15 +1,15 @@
 export const toggleSelect = (selectElement) => {
   selectElement.addEventListener('click', (e) => {
-    selectElement.classList.toggle('button--select-active');
+    selectElement.classList.toggle('select-active');
 
     const optionElement = e.target.closest('ul li');
     if (!optionElement) return;
 
-    selectElement.querySelector('.button__text-fixed').textContent =
+    selectElement.querySelector('span').textContent =
       optionElement.querySelector('span').textContent;
   });
 
   selectElement.addEventListener('blur', () => {
-    selectElement.classList.remove('button--select-active');
+    selectElement.classList.remove('select-active');
   });
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import { toggleSelect } from './components/select';
 
 const selectHero = document.querySelector('.header__select');
+const selectJoinNow = document.querySelector('.join-now__form-card-select');
 
 toggleSelect(selectHero);
+toggleSelect(selectJoinNow);
